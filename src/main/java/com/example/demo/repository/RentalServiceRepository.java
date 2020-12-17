0db8479657cdc197337cf621539d308ee4332763
cuda <ventusxu0905@gmail.com>
2020-12-17 01:10:03 -0500

    fix embeddedID

--- src/main/java/com/example/demo/repository/RentalServiceRepository.java
+++ src/main/java/com/example/demo/repository/RentalServiceRepository.java
@@ -1,10 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.RentalService;
+import com.example.demo.domain.support.RentalServiceId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface RentalServiceRepository extends JpaRepository<RentalService, Long> {
+public interface RentalServiceRepository extends JpaRepository<RentalService, RentalServiceId> {
     RentalService getRentalServiceByInvoiceEquals(Long id);
 
     RentalService findByCustomer(@Param("id") Long id);
